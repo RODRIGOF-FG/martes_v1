7846949ca6abdadc2701cd3ac920056cae8f02c6
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-10 21:09:11 -0500

    cambio 3

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/Incidencia.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/Incidencia.java
@@ -4,6 +4,33 @@ import com.example.sistema_procesos.User.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "incidencias")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Incidencia {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String nombreUsuario;
+
+    private String descripcion;
+
+    private String estado; // PENDIENTE / ATENDIDO
+
+    private LocalDateTime fechaRegistro;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    @JsonIgnore
+    private User usuario;
 }
