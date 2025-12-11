504f2498c8052d7d96e2e6b5fca54e0238c7a9aa
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-10 21:12:58 -0500

    cambio 5

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaRepository.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaRepository.java
@@ -1,5 +1,11 @@
 package com.example.sistema_procesos.registrar_incidencia;
 
-public class IncidenciaRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IncidenciaRepository extends JpaRepository<Incidencia, Long> {
+    List<Incidencia> findByUserId(Long userId);
+    Optional<Incidencia> findByIdAndUserId(Long id, Long userId);
 }
