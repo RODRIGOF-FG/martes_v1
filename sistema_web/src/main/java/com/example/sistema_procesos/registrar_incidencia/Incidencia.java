9e5400fe6734d539d178d2f5f03e1a485a1b51fb
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-10 21:08:29 -0500

    cambio 2

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/Incidencia.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/Incidencia.java
@@ -1,5 +1,9 @@
 package com.example.sistema_procesos.registrar_incidencia;
 
+import com.example.sistema_procesos.User.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
 public class Incidencia {
     
 }
