a8f70e50bbf993c08978edb0390ea823653bf11b
heidyAlisson03 <heidysolis487@gmail.com>
2025-12-10 23:21:27 -0500

    proyecto

--- sistema_web/src/main/java/com/example/sistema_procesos/Config/SecurityConfig.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/Config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                         .requestMatchers("/admin/asignar/mi-asignacion", "/admin/asignar/completar").hasAnyRole("ADMIN", "OPERADOR")
                       
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-
+                        .requestMatchers("/admin/salida/").hasRole("ADMIN")	
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
