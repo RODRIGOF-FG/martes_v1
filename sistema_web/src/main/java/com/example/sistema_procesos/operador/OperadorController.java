4f1a28a929b425a5aed7a0b4ed0b4cc2dc8b8bb5
heidyAlisson03 <heidysolis487@gmail.com>
2025-12-10 23:53:56 -0500

    creacion de nueva clase operador importacion de libreria

---
+++ sistema_web/src/main/java/com/example/sistema_procesos/operador/OperadorController.java
@@ -0,0 +1,6 @@
+import com.example.sistema_procesos.User.User;
+import com.example.sistema_procesos.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
