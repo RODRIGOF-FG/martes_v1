1c92a117d26a9ca0f0655d5e7eaba2b78a3efc27
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-10 21:21:17 -0500

    cambio 9 H21 terminada

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaController.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaController.java
@@ -1,5 +1,22 @@
 package com.example.sistema_procesos.registrar_incidencia;
 
+import com.example.sistema_procesos.User.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/operador/incidencias")
+@RequiredArgsConstructor
 public class IncidenciaController {
-    
+
+    private final IncidenciaService service;
+
+    @PostMapping
+    public Incidencia registrar(@AuthenticationPrincipal User user,
+                                @RequestParam String descripcion) {
+        return service.registrarIncidencia(user, descripcion);
+    }
 }
