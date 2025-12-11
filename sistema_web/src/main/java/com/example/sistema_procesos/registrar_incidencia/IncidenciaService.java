a95b60cb0d7c0fbbabd19f7e6f0700dd6bed0f96
RODRIGOF-FG <ffloriang@autonoma.edu.pe>
2025-12-10 21:17:13 -0500

    cambio 7

--- sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaService.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/registrar_incidencia/IncidenciaService.java
@@ -1,5 +1,30 @@
 package com.example.sistema_procesos.registrar_incidencia;
 
+import com.example.sistema_procesos.User.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class IncidenciaService {
-    
+
+    private final IncidenciaRepository repository;
+
+    // Registro
+    public Incidencia registrarIncidencia(User user, String descripcion) {
+        Incidencia incidencia = Incidencia.builder()
+                .userId(user.getId())
+                .nombreUsuario(user.getFirstname() + " " + user.getLastname())
+                .descripcion(descripcion)
+                .estado("PENDIENTE")
+                .fechaRegistro(LocalDateTime.now())
+                .usuario(user)
+                .build();
+
+        return repository.save(incidencia);
+    }
+
 }
