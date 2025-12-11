9cab69ad11d0a110d54cf42483b382f53f2b370d
HectorTinoco119 <htinocor@autonoma.edu.pe>
2025-12-10 23:46:52 -0500

    crear clase adminincidencia y importarlibrerias

---
+++ sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaController.java
@@ -0,0 +1,5 @@
+import com.example.sistema_procesos.registrar_incidencia.Incidencia;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
