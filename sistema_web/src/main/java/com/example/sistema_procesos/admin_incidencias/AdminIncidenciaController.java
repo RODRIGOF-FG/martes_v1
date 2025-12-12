0442d362d8bb2c1609b41189f798717780ad914b
HectorTinoco119 <htinocor@autonoma.edu.pe>
2025-12-12 17:39:03 -0500

    declaracion de la clase con el atrivuto servicio

--- sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaController.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaController.java
@@ -1,3 +1,4 @@
+package com.example.sistema_procesos.admin_incidencias;
 import com.example.sistema_procesos.registrar_incidencia.Incidencia;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -6,4 +7,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/incidencias")
 @RequiredArgsConstructor
+public class AdminIncidenciaController {
+
+    private final AdminIncidenciaService service;
+
+}
 
