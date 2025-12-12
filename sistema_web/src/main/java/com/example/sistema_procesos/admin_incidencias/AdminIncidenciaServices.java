4f945df5e80b97072f3ddb6ea74f0458c99095ec
Yoyialx <yordialvaradovasquez@gmail.com>
2025-12-11 21:26:24 -0500

    clase de AdminService agregado

--- sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaServices.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaServices.java
@@ -1,6 +1,14 @@
+package com.example.sistema_procesos.admin_incidencias;
+
 import com.example.sistema_procesos.registrar_incidencia.Incidencia;
 import com.example.sistema_procesos.registrar_incidencia.IncidenciaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminIncidenciaService {
+
+}
