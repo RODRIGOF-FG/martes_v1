cf8764fa34421612e9fe07f4d10d9cdaa8a0354b
Yoyialx <yordialvaradovasquez@gmail.com>
2025-12-11 21:38:04 -0500

    agregar comentario

--- sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaServices.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaServices.java
@@ -6,14 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-<<<<<<< HEAD
 @Service
 @RequiredArgsConstructor
 public class AdminIncidenciaService {
 
+// admin incidencias
+
 }
-=======
-@RestController
-@RequestMapping("/admin/incidencias")
-@RequiredArgsConstructor
->>>>>>> 72ad4cb5508588049b59061caac357a7d8cb3d7c
