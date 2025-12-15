50f49741bb132165a2a886772a0ed521c37ea9c4
HectorTinoco119 <htinocor@autonoma.edu.pe>
2025-12-14 19:51:09 -0500

    CODIGO TERMINADO DE ADMIN INCIDENCIA

--- sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaController.java
+++ sistema_web/src/main/java/com/example/sistema_procesos/admin_incidencias/AdminIncidenciaController.java
@@ -1,20 +1,25 @@
 package com.example.sistema_procesos.admin_incidencias;
+
 import com.example.sistema_procesos.registrar_incidencia.Incidencia;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/admin/incidencias")
 @RequiredArgsConstructor
 public class AdminIncidenciaController {
 
     private final AdminIncidenciaService service;
+
     @GetMapping
     public List<Incidencia> listarTodas() {
-    return service.listarTodas();
+        return service.listarTodas();
+    }
+
+    @PatchMapping("/{id}/atendida")
+    public Incidencia marcarAtendida(@PathVariable Long id) {
+        return service.marcarAtendida(id);
+    }
 }
-
-
-}
-
